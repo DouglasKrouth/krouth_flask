49889141ac78644ea26a650dc6d2001d41668f82
DouglasKrouth <douglaskrouth@gmail.com>
2024-03-11 15:10:35 -0500

    update

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import krouth_site.views as views
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.add_url_rule('/', view_func=views.index)
+app.add_url_rule('/hello', view_func=views.hello)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
---
+++ krouth_site/views.py
@@ -0,0 +1,7 @@
+from flask import render_template
+
+def index():
+    return 'Index Page'
+
+def hello():
+    return 'Hello, World'
